6363d16ec300bf3e3e077fda77840a7e1880324b
rodrigo-frenk <furenku@gmail.com>
2016-04-04 22:17:01 -0500

    maquetacion blog

--- maquetacion01/js/app.js
+++ maquetacion01/js/app.js
@@ -1,1 +1,30 @@
 $(document).foundation();
+
+/*
+selectores
+
+por etiqueta: p. ej. "div"
+por clase, p.ej. ".row"
+por identidad, p. ej. "#caja_principal"
+
+jerarquía "ul>li"
+
+etc etc
+
+*/
+
+
+
+var selector = '.imgLiquid.imgLiquidFill';
+$( selector ).imgLiquid();
+
+// var otroselector = '.imgLiquid.imgLiquidNoFill';
+// $( otroselector ).imgLiquid({ fill: false });
+//
+// // ejemplo de un Objeto de Javascript ( Object )
+// var persona = {
+//    nombre: "Juan",
+//    apellido: "Pérez",
+//    edad: 34,
+//    ocupacion: "Funcionario"
+// };
